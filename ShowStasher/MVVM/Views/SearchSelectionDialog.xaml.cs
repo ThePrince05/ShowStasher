877898ba3b9a200a92108d2f8cb050cd74caac69
Prince <princesithole49@gmail.com>
2026-07-05 06:08:31 +0200

    Fix preview selection

--- ShowStasher/MVVM/Views/SearchSelectionDialog.xaml.cs
+++ ShowStasher/MVVM/Views/SearchSelectionDialog.xaml.cs
@@ -34,5 +34,34 @@ namespace ShowStasher.MVVM.Views
             }
         }
 
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeRestore_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
