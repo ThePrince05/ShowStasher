30ec9f5c53a57993a4d8ef3aa153e85ecdc8e688
Prince <princesithole49@gmail.com>
2026-07-05 05:24:42 +0200

    Add progressbar to taskbar

---
+++ ShowStasher/Converters/BusyToTaskbarStateConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Shell;
+namespace ShowStasher.Converters
+{
+    // Converts bool IsBusy to TaskbarItemProgressState
+    public class BusyToTaskbarStateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isBusy && isBusy)
+            {
+                return TaskbarItemProgressState.Normal;
+            }
+            return TaskbarItemProgressState.None;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ShowStasher/Converters/ProgressToTaskbarValueConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Shell;
+
+namespace ShowStasher.Converters
+{
+    // Converts 0-100 int to 0.0-1.0 double
+    public class ProgressToTaskbarValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int intValue)
+            {
+                return intValue / 100.0;
+            }
+            return 0.0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+   
+}
